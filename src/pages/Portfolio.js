c6387855458cd3f1a7600af83f42956715670261
Wasif Mahmood <116950247+wasifmahmood@users.noreply.github.com>
2023-08-30 15:27:06 +0500

    image styling

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import './Portfolio.css'
 import { COLORS } from '../constants/colors';
+import { GrFormClose } from 'react-icons/gr';
+// Fashion 
 import F1 from '../image/fashion/F1.jpg';
 import F2 from '../image/fashion/F2.jpg';
 import F3 from '../image/fashion/F3.jpg';
 import F4 from '../image/fashion/F4.jpg';
+// Wedding
 import W1 from '../image/wedding/W1.jpg';
 import W2 from '../image/wedding/W2.jpg';
 import W3 from '../image/wedding/W3.jpg';
@@ -13,6 +16,8 @@ import W5 from '../image/wedding/W5.jpg';
 import W6 from '../image/wedding/W6.jpg';
 import W7 from '../image/wedding/W7.jpg';
 import W8 from '../image/wedding/W8.jpg';
+// product
+// Mask kitchen
 import MK1 from '../image/product/mask/MK1.jpg';
 import MK2 from '../image/product/mask/MK2.jpg';
 import MK3 from '../image/product/mask/MK3.jpg';
@@ -21,6 +26,7 @@ import MK5 from '../image/product/mask/MK5.jpg';
 import MK6 from '../image/product/mask/MK6.jpg';
 import MK7 from '../image/product/mask/MK7.jpg';
 import MK8 from '../image/product/mask/MK8.jpg';
+// Garam Chai
 import G1 from '../image/product/GC/G1.jpeg';
 import G2 from '../image/product/GC/G2.jpeg';
 import G3 from '../image/product/GC/G3.jpeg';
@@ -29,20 +35,39 @@ import G5 from '../image/product/GC/G5.jpeg';
 import G6 from '../image/product/GC/G6.jpeg';
 import G7 from '../image/product/GC/G7.jpeg';
 import G8 from '../image/product/GC/G8.jpeg';
-import MF1 from '../image/product/momin/MF1.jpg';
-import MF2 from '../image/product/momin/MF2.jpg';
-import MF3 from '../image/product/momin/MF3.jpg';
-import MF4 from '../image/product/momin/MF4.jpg';
-import MF5 from '../image/product/momin/MF5.jpg';
-import MF6 from '../image/product/momin/MF6.jpg';
-import MF7 from '../image/product/momin/MF7.jpg';
-import MF8 from '../image/product/momin/MF8.jpg';
-import Lightbox from 'react-image-lightbox';
-import 'react-image-lightbox/style.css';
-
+// Golden Dairy
+import MF1 from '../image/product/GoldenDairy/GD1.jpg';
+import MF2 from '../image/product/GoldenDairy/GD2.jpg';
+import MF3 from '../image/product/GoldenDairy/GD3.jpg';
+import MF4 from '../image/product/GoldenDairy/GD4.jpg';
+import MF5 from '../image/product/GoldenDairy/GD5.jpg';
+import MF6 from '../image/product/GoldenDairy/GD6.jpg';
+import MF7 from '../image/product/GoldenDairy/GD7.jpg';
+import MF8 from '../image/product/GoldenDairy/GD8.jpg';
+// Momin FOOD
 
 const Portfolio = () => {
-    const images = [
+    const Fashion = [
+        { src: F1, title: 'Image 1' },
+        { src: F2, title: 'Image 2' },
+        { src: F3, title: 'Image 3' },
+        { src: F4, title: 'Image 4' },
+        { src: F4, title: 'Image 5' },
+        { src: F3, title: 'Image 6' },
+        { src: F2, title: 'Image 7' },
+        { src: F1, title: 'Image 8' },
+    ]
+    const Wedding = [
+        { src: W1, title: 'Image 1' },
+        { src: W2, title: 'Image 5' },
+        { src: W3, title: 'Image 3' },
+        { src: W4, title: 'Image 4' },
+        { src: W5, title: 'Image 5' },
+        { src: W6, title: 'Image 6' },
+        { src: W7, title: 'Image 7' },
+        { src: W8, title: 'Image 8' },
+    ]
+    const images1 = [
         { src: MK1, title: 'Image 1' },
         { src: MK2, title: 'Image 2' },
         { src: MK3, title: 'Image 3' },
@@ -53,11 +78,46 @@ const Portfolio = () => {
         { src: MK8, title: 'Image 8' },
         // ...other image objects
     ];
-     const openImageInModal = (src) => {
-        const newWindow = window.open('', '_blank');
-        newWindow.document.write(`<img src="${src}" alt="Image" />`);
+    const images2 = [
+        { src: G1, title: 'Image 1' },
+        { src: G2, title: 'Image 2' },
+        { src: G3, title: 'Image 3' },
+        { src: G4, title: 'Image 4' },
+        { src: G5, title: 'Image 5' },
+        { src: G6, title: 'Image 6' },
+        { src: G7, title: 'Image 7' },
+        { src: G8, title: 'Image 8' },
+    ];
+    const images3 = [
+        { src: MF1, title: 'Image 1' },
+        { src: MF2, title: 'Image 2' },
+        { src: MF3, title: 'Image 3' },
+        { src: MF4, title: 'Image 4' },
+        { src: MF5, title: 'Image 5' },
+        { src: MF6, title: 'Image 6' },
+        { src: MF7, title: 'Image 7' },
+        { src: MF8, title: 'Image 8' },
+    ];
+    const image4 = [
+        { src: MF1, title: 'Image 1' },
+        { src: MF2, title: 'Image 2' },
+        { src: MF3, title: 'Image 3' },
+        { src: MF4, title: 'Image 4' },
+        { src: MF5, title: 'Image 5' },
+        { src: MF6, title: 'Image 6' },
+        { src: MF7, title: 'Image 7' },
+        { src: MF8, title: 'Image 8' },
+    ];
+
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const openImageModal = (src) => {
+        setSelectedImage(src);
     };
 
+    const closeImageModal = () => {
+        setSelectedImage(null);
+    };
     return (
         <>
             <div className="fashion-main p-4" style={{ backgroundColor: COLORS.primary, color: COLORS.yellow }}>
@@ -66,14 +126,26 @@ const Portfolio = () => {
                         <h2>FASHION SHOTS</h2>
                     </div>
                     <div className="image-container  pt-4">
-                        <img className="fimg" src={F1} alt="Logo White" />
-                        <img className="fimg" src={F2} alt="Logo Black" />
-                        <img className="fimg" src={F3} alt="Logo White" />
-                        <img className="fimg" src={F4} alt="Logo Black" />
-                        <img className="fimg" src={F4} alt="Logo Black" />
-                        <img className="fimg" src={F3} alt="Logo White" />
-                        <img className="fimg" src={F2} alt="Logo Black" />
-                        <img className="fimg" src={F1} alt="Logo White" />
+                        {Fashion.map((image, index) => (
+                            <img
+                                key={index}
+                                src={image.src}
+                                className="fimg"
+                                alt={image.title}
+                                onClick={() => openImageModal(image.src)}
+                            />
+                        ))}
+
+                        {selectedImage && (
+                            <div className="image-modal-overlay">
+                                <div className="image-modal">
+                                    <button onClick={closeImageModal} className="close-button">
+                                        <GrFormClose size={30} />
+                                    </button>
+                                    <img src={selectedImage} alt="Selected" />
+                                </div>
+                            </div>
+                        )}
                     </div>
                 </div>
                 <hr className='solid hr'></hr>
@@ -82,14 +154,26 @@ const Portfolio = () => {
                         <h2>WEDDING PHOTO</h2>
                     </div>
                     <div className="image-container  pt-4">
-                        <img className="fimg" src={W1} alt="Logo White" />
-                        <img className="fimg" src={W2} alt="Logo Black" />
-                        <img className="fimg" src={W3} alt="Logo White" />
-                        <img className="fimg" src={W4} alt="Logo Black" />
-                        <img className="fimg" src={W5} alt="Logo Black" />
-                        <img className="fimg" src={W6} alt="Logo Black" />
-                        <img className="fimg" src={W7} alt="Logo Black" />
-                        <img className="fimg" src={W8} alt="Logo Black" />
+                        {Wedding.map((image, index) => (
+                            <img
+                                key={index}
+                                src={image.src}
+                                className="fimg"
+                                alt={image.title}
+                                onClick={() => openImageModal(image.src)}
+                            />
+                        ))}
+
+                        {selectedImage && (
+                            <div className="image-modal-overlay">
+                                <div className="image-modal">
+                                    <button onClick={closeImageModal} className="close-button">
+                                        <GrFormClose size={30} />
+                                    </button>
+                                    <img src={selectedImage} alt="Selected" />
+                                </div>
+                            </div>
+                        )}
                     </div>
                 </div>
                 <hr className='solid hr'></hr>
@@ -98,48 +182,72 @@ const Portfolio = () => {
                         <h2>Product Photography</h2>
                     </div>
                     <div className="d-flex client1 p-4">
-                        <div className="clientleft">
+                        <div className="clientleft ">
                             <h2>Mask's Kitchen</h2>
                             <span>Marks Kitchen Is The Best Fast-Food Restaurant In Lahore, Bringing You A Variety Of Food  From Around The World.</span>
-                            <span>
+                            <div className="pt-4">
                                 <a href="https://markskitchen.com.pk/">
                                     <button type="button" className="btn btn-warning">Marks Kitchen</button>
                                 </a>
-                            </span>
+                            </div>
                         </div>
                         <div className="d-flex clientright">
-                            {images.map((image, index) => (
+                            {images1.map((image, index) => (
                                 <img
                                     key={index}
                                     src={image.src}
                                     className="imgclient1"
                                     alt={image.title}
-                                    onClick={() => openImageInModal(image.src)}
+                                    onClick={() => openImageModal(image.src)}
                                 />
                             ))}
+
+                            {selectedImage && (
+                                <div className="image-modal-overlay">
+                                    <div className="image-modal">
+                                        <button onClick={closeImageModal}
+                                            className="close-button"
+                                        >
+                                            <GrFormClose size={30} />
+                                        </button>
+                                        <img src={selectedImage} alt="Selected" />
+                                    </div>
+                                </div>
+                            )}
                         </div>
                     </div>
                     <hr className='solid hr'></hr>
                     <div className="d-flex client2 p-4" >
                         <div className="d-flex clientright">
-                            <img src={G1} className="imgclient1 " />
-                            <img src={G2} className="imgclient1" />
-                            <img src={G3} className="imgclient1" />
-                            <img src={G4} className="imgclient1" />
-                            <img src={G5} className="imgclient1" />
-                            <img src={G6} className="imgclient1" />
-                            <img src={G7} className="imgclient1" />
-                            <img src={G8} className="imgclient1" />
+                            {images2.map((image, index) => (
+                                <img
+                                    key={index}
+                                    src={image.src}
+                                    className="imgclient1"
+                                    alt={image.title}
+                                    onClick={() => openImageModal(image.src)}
+                                />
+                            ))}
 
+                            {selectedImage && (
+                                <div className="image-modal-overlay">
+                                    <div className="image-modal">
+                                        <button onClick={closeImageModal} className="close-button">
+                                            <GrFormClose size={30} />
+                                        </button>
+                                        <img src={selectedImage} alt="Selected" />
+                                    </div>
+                                </div>
+                            )}
                         </div>
                         <div className="clientleft">
                             <h2>Garam Chai</h2>
                             <span>Garam Chai Is The Best Fast-Food Restaurant In Lahore, Bringing You A Variety Of Food  From Around The World.</span>
-                            <span >
+                            <div className="pt-4">
                                 <a href="https://www.instagram.com/garamchaipk/">
                                     <button type="button" className="btn btn-warning">Garam Chai</button>
                                 </a>
-                            </span>
+                            </div>
                         </div>
                     </div>
                     <hr className='solid hr'></hr>
@@ -147,44 +255,67 @@ const Portfolio = () => {
                         <div className="clientleft">
                             <h2>Golden Dairy</h2>
                             <span>Desi Ghee is also an excellent source of Vitamin E. Studies have shown that Vitamin E has significant antioxidant properties. Antioxidants like Vitamin E have been linked to lowering the risk of cancer, arthritis, and cataracts. Vitamin E can also help reduce the risk of heart disease.</span>
-                            <span >
+                            <div className="pt-4">
                                 <a href="https://mygoldendairy.com/">
                                     <button type="button" className="btn btn-warning">Golden Dairy</button>
                                 </a>
-                            </span>
+                            </div>
                         </div>
                         <div className="d-flex clientright">
-                            <img src={MF1} className="imgclient1" />
-                            <img src={MF2} className="imgclient1" />
-                            <img src={MF3} className="imgclient1" />
-                            <img src={MF4} className="imgclient1" />
-                            <img src={MF5} className="imgclient1" />
-                            <img src={MF6} className="imgclient1" />
-                            <img src={MF7} className="imgclient1" />
-                            <img src={MF8} className="imgclient1" />
+                            {images3.map((image, index) => (
+                                <img
+                                    key={index}
+                                    src={image.src}
+                                    className="imgclient1"
+                                    alt={image.title}
+                                    onClick={() => openImageModal(image.src)}
+                                />
+                            ))}
 
+                            {selectedImage && (
+                                <div className="image-modal-overlay">
+                                    <div className="image-modal">
+                                        <button onClick={closeImageModal} className="close-button">
+                                            <GrFormClose size={30} />
+                                        </button>
+                                        <img src={selectedImage} alt="Selected" />
+                                    </div>
+                                </div>
+                            )}
                         </div>
                     </div>
                     <hr className='solid hr'></hr>
                     <div className="d-flex client4 p-4">
                         <div className="d-flex clientright">
-                            <img src={MF1} className="imgclient1" />
-                            <img src={MF2} className="imgclient1" />
-                            <img src={MF3} className="imgclient1" />
-                            <img src={MF4} className="imgclient1" />
-                            <img src={MF5} className="imgclient1" />
-                            <img src={MF6} className="imgclient1" />
-                            <img src={MF7} className="imgclient1" />
-                            <img src={MF8} className="imgclient1" />
+                            {image4.map((image, index) => (
+                                <img
+                                    key={index}
+                                    src={image.src}
+                                    className="imgclient1"
+                                    alt={image.title}
+                                    onClick={() => openImageModal(image.src)}
+                                />
+                            ))}
+
+                            {selectedImage && (
+                                <div className="image-modal-overlay">
+                                    <div className="image-modal">
+                                        <button onClick={closeImageModal} className="close-button">
+                                            <GrFormClose size={30} />
+                                        </button>
+                                        <img src={selectedImage} alt="Selected" />
+                                    </div>
+                                </div>
+                            )}
                         </div>
                         <div className="clientleft">
                             <h2>Momin FOOD</h2>
                             <span>We are a proud suppliers of Food Masala flavors and spices. Experience the high-quality of our full range of dry products.</span>
-                            <span >
+                            <div className="pt-4">
                                 <a href="https://mominfoods.com/">
                                     <button type="button" className="btn btn-warning">Momin FOOD</button>
                                 </a>
-                            </span>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -7,47 +7,51 @@ import { SiAdobelightroom, SiAdobepremierepro, SiAdobephotoshop } from "react-ic
 const Service = () => {
     return (
         <>
-            <div className="service"
-                style={{ backgroundColor: COLORS.primary, color: COLORS.yellow }}>
-                <div className="d-flex">
-                    <div className="left text-center ">
-                        <h1>SERVICES</h1>
-                        <span>"Elecvting Your Vision,Frame by Frame"</span>
-                    </div>
-                    <div className="right row">
-                        <div className="col-6">
-                            <span className="d-block icon"><AiOutlineForward size={30} style={{ paddingRight: '10px' }} />Wedding Photography</span>
-                            <span className="d-block icon"><AiOutlineForward size={30} style={{ paddingRight: '10px' }} />Event Photography</span>
-                            <span className="d-block icon"><AiOutlineForward size={30} style={{ paddingRight: '10px' }} />Corporate Event Photography</span>
-                            <span className="d-block icon"><AiOutlineForward size={30} style={{ paddingRight: '10px' }} />Fashion Photography</span>
+            <div className="Main">
+                <div className="service"
+                    style={{ backgroundColor: COLORS.primary, color: COLORS.yellow }}>
+                    <div className="d-flex">
+                        <div className="left text-center ">
+                            <h1>SERVICES</h1>
+                            <span>"Elecvting Your Vision,Frame by Frame"</span>
                         </div>
-                        <div className="col-6">
-                            <span className="d-block icon"><AiOutlineForward size={30} style={{ paddingRight: '10px' }} />Photography</span>
-                            <span className="d-block icon"><AiOutlineForward size={30} style={{ paddingRight: '10px' }} />Photography</span>
-                            <span className="d-block icon"><AiOutlineForward size={30} style={{ paddingRight: '10px' }} />Photography</span>
-                            <span className="d-block icon"><AiOutlineForward size={30} style={{ paddingRight: '10px' }} />Photography</span>
+                        <div className="right row">
+                            <div className="col-6">
+                                <span className="d-block icon"><AiOutlineForward size={30} style={{ paddingRight: '10px' }} />Wedding Photography</span>
+                                <span className="d-block icon"><AiOutlineForward size={30} style={{ paddingRight: '10px' }} />Event Photography</span>
+                                <span className="d-block icon"><AiOutlineForward size={30} style={{ paddingRight: '10px' }} />Corporate Event Photography</span>
+                                <span className="d-block icon"><AiOutlineForward size={30} style={{ paddingRight: '10px' }} />Fashion Photography</span>
+                            </div>
+                            <div className="col-6">
+                                <span className="d-block icon"><AiOutlineForward size={30} style={{ paddingRight: '10px' }} />Photography</span>
+                                <span className="d-block icon"><AiOutlineForward size={30} style={{ paddingRight: '10px' }} />Photography</span>
+                                <span className="d-block icon"><AiOutlineForward size={30} style={{ paddingRight: '10px' }} />Photography</span>
+                                <span className="d-block icon"><AiOutlineForward size={30} style={{ paddingRight: '10px' }} />Photography</span>
+                            </div>
+                        </div>
+                        <div style={{ backgroundImage: "url('../image/service.jpg')" }}></div>
+
+
+                    </div>
+                </div>
+                <div className="expertise" style={{ backgroundColor: COLORS.yellow, color: COLORS.primary }}>
+                    <div className="left text-center">
+                        <h1>EXPERTISE</h1>
+                        <span>Main Software</span>
+                    </div>
+                    <div className="right row pt-4">
+                        <div className="col-4">
+                            <span className="d-block"><SiAdobelightroom size={40} style={{ paddingRight: '10px' }} />Lightroom</span>
+                        </div>
+                        <div className="col-4">
+                            <span className="d-block"><SiAdobepremierepro size={40} style={{ paddingRight: '10px' }} />Adobe Premiere Pro</span>
+                        </div>
+                        <div className="col-4">
+                            <span className="d-block"><SiAdobephotoshop size={40} style={{ paddingRight: '10px' }} />PhotoShop</span>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="expertise" style={{ backgroundColor: COLORS.yellow, color: COLORS.primary }}>
-                <div className="left text-center">
-                    <h1>EXPERTISE</h1>
-                    <span>Main Software</span>
-                </div>
-                <div className="right row pt-4">
-                    <div className="col-4">
-                        <span className="d-block"><SiAdobelightroom size={40} style={{ paddingRight: '10px' }} />Lightroom</span>
-                    </div>
-                    <div className="col-4">
-                        <span className="d-block"><SiAdobepremierepro size={40} style={{ paddingRight: '10px' }} />Adobe Premiere Pro</span>
-                    </div>
-                    <div className="col-4">
-                        <span className="d-block"><SiAdobephotoshop size={40} style={{ paddingRight: '10px' }} />PhotoShop</span>
-                    </div>
-                </div>
-            </div>
-            
         </>
     );
 }
